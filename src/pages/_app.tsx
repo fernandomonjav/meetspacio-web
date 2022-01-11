01d80a216a7757192dbf390d37ae631f8dece56f
Fernando Monja Vasquez <fernandomonjav@gmail.com>
2022-01-10 21:58:05 -0500

    Init project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from 'next/app'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const getLayout = (Component as any).getLayout || ((page: React.ReactNode) => page)
+
+  return getLayout(<Component {...pageProps} />)
+}
+
+export default MyApp
